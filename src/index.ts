93a15291f1d2b149920ec48eeaa216e26fe49683
Oussama Ajala <115725343+mlgknight@users.noreply.github.com>
2025-06-07 17:01:02 -0400

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import type { Request, Response } from 'express';
+import { notes, noteRouter } from './routes/note.routes.ts';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/notes', noteRouter);
+
+app.get('/api/notes', (req: Request, res: Response) => {
+	res.json(notes);
+});
+
+app.listen(PORT, () => {
+	console.log(`Server running on Port ${PORT}`);
+});
